fd0618b04b38ebbf7782c8e68c5bf8855fd84d8d
abraham <abraham.paniagua@gmail.com>
2016-08-08 10:20:10 -0700

    Added linux-dash to repo

---
+++ linux-dash/app/server/index.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import os
+from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer, test as _test
+import subprocess
+from SocketServer import ThreadingMixIn
+
+modulesSubPath = '/server/linux_json_api.sh'
+appRootPath = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    pass
+
+class MainHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        try:
+            data = ''
+            contentType = 'text/html'
+            if self.path.startswith("/server/"):
+                module = self.path.split('=')[1]
+                output = subprocess.Popen(
+                    appRootPath + modulesSubPath + " " + module,
+                    shell = True,
+                    stdout = subprocess.PIPE)
+                data = output.communicate()[0]
+            else:
+                if self.path == '/':
+                    self.path = 'index.html'
+                f = open(appRootPath + os.sep + self.path)
+                data = f.read()
+                if self.path.startswith('/main.css'):
+                    contentType = 'text/css'
+                f.close()
+            self.send_response(200)
+            self.send_header('Content-type', contentType)
+            self.end_headers()
+            self.wfile.write(data)
+
+        except IOError:
+            self.send_error(404, 'File Not Found: %s' % self.path)
+
+if __name__ == '__main__':
+    server = ThreadedHTTPServer(('0.0.0.0', 80), MainHandler)
+    print 'Starting server, use <Ctrl-C> to stop'
+    server.serve_forever()
---
+++ linux-dash/src/js/core/app.js
@@ -0,0 +1,17 @@
+function runFn(server, $location, $rootScope) {
+
+  server.checkIfWebsocketsAreSupported()
+
+  var currentRoute = $location.path()
+  var currentTab = (currentRoute === '/loading')? 'system-status': currentRoute
+  localStorage.setItem('currentTab', currentTab)
+
+  $location.path('/loading')
+}
+
+angular
+  .module('linuxDash', ['ngRoute'])
+  .run([ 'server', '$location', '$rootScope', runFn])
+  .config(['$compileProvider', function ($compileProvider) {
+    $compileProvider.debugInfoEnabled(false)
+  }])
